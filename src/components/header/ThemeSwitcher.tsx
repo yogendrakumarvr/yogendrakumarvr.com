fe95c9430fee865b81c574c06542e162c897a0c2
Yogendra Kumar V R <yogendra.kvr@surya-digital.com>
2023-09-13 21:40:06 +0530

    Add theme switcher

---
+++ src/components/header/ThemeSwitcher.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { LightMode } from '../../assets/LightMode';
+import { DarkMode } from '../../assets/DarkMode';
+
+export const ThemeSwitcher = () => {
+  const [theme, setTheme] = useState('light');
+
+  const getDefaultTheme = () => {
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    }
+    return 'light';
+  };
+
+  useEffect(() => {
+    const defaultTheme = getDefaultTheme();
+    if (defaultTheme === 'dark') {
+      const htmlElement = document.querySelector('html');
+      htmlElement?.classList.add('dark');
+      localStorage.setItem('theme', defaultTheme);
+    }
+    setTheme(defaultTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const updatedTheme = theme === 'dark' ? 'light' : 'dark';
+    const htmlElement = document.querySelector('html');
+    htmlElement?.classList.toggle('dark');
+    localStorage.setItem('theme', updatedTheme);
+    setTheme(updatedTheme);
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      {theme === 'dark' ? (
+        <LightMode className="w-[32px] fill-light lg:w-[48px]" />
+      ) : (
+        <DarkMode className="w-[32px] fill-dark lg:w-[48px]" />
+      )}
+    </button>
+  );
+};
