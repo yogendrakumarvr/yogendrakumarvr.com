85605a5ec3fe0d98c89c7667317017ee1756aa54
Yogendra Kumar V R <yogendra.kvr@surya-digital.com>
2023-09-14 10:32:18 +0530

    Add Resume

--- src/assets/DarkMode.tsx
+++ src/assets/DarkMode.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const DarkMode = ({ className }: { className: string }) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" className={className}>
-      <path d="M480-120q-151 0-255.5-104.5T120-480q0-138 90-239.5T440-838q25-3 39 18t-1 44q-17 26-25.5 55t-8.5 61q0 90 63 153t153 63q31 0 61.5-9t54.5-25q21-14 43-1.5t19 39.5q-14 138-117.5 229T480-120Z"></path>
+      <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z" />
     </svg>
   );
 };
